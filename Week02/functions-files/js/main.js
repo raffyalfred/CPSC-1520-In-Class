875c142ee326e61dab3706650b94e27af3656518
raffyalfred <35229077+raffyalfred@users.noreply.github.com>
2018-01-15 09:15:10 -0700

    created an updateInnerHTML function

---
+++ Week02/functions-files/js/main.js
@@ -0,0 +1,16 @@
+/* main.js
+    This is a comment block, enclosed in a slash + asterisk
+    and ending with an asterisk + slash
+*/
+
+// Demo of calling a function - console is the javascript editor environment in the browser
+// console.log(message) displays the message in the console tab of the browser
+console.log('main.js is loaded');
+
+// The updateInnerHTML function has two parameters:
+// - parameter list - is a comma-separated list of variable names
+// - selector - is a string that identifies which DOM element to selector
+// - newValue - is a string that will be the new value for the selectors .innerHTML
+function updateInnerHTML(selector, newValue) {
+  document.querySelector(selector).innerHTML = newValue;
+}
