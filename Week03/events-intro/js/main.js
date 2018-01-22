6bad756466be5f699f5221bce7bb89c770fc62aa
raffyalfred <35229077+raffyalfred@users.noreply.github.com>
2018-01-22 08:55:47 -0700

    added handleFeatureClickLink

--- Week03/events-intro/js/main.js
+++ Week03/events-intro/js/main.js
@@ -1,5 +1,6 @@
 // 1. select the target element
 var featureImage = document.querySelector('img.feature');
+var featureLink = document.querySelector('a.feature.link');
 
 // 2. create the event listener function
 
@@ -14,5 +15,20 @@ function handleFeatureClick() {
 
 
 }
+
+function handleFeatureLinkClick(evt) {
+    console.log('a.feature.link was clicked....');
+    // set the image src to the anchor's href value
+    featureImage.src = featureLink.href;
+
+    // make the image visible
+    featureImage.classList.remove('hidden');
+
+    // do not load the image in the page
+    evt.preventDefault();
+
+}
+
 // 3. add event listener
 featureImage.addEventListener('click', handleFeatureClick);
+featureLink.addEventListener('click', handleFeatureLinkClick);
