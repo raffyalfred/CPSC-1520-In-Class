d3838fe2194c96a098dfa0a81e2e0634234e789b
raffyalfred <35229077+raffyalfred@users.noreply.github.com>
2018-01-26 09:13:04 -0700

    event-listener-lab

---
+++ Week03/EventsAssessment/js/main.js
@@ -0,0 +1,38 @@
+// Note: The code here is presuming that this script is loaded at the END of the HTML Body tag.
+
+// Step 1) Select the link
+//  featureLink will reference an anchor tag
+var featureLink = document.querySelector('a.link.feature');
+
+
+// Step 2) Create the event listener function (aka event handler)
+function featureLinkHandler(evt) { //evt parameter is an object representing the event as it is passed to our method
+    //  featureImage will reference an image tag
+    var featureImage = document.querySelector('img.feature');
+    featureImage.src = featureLink.href;
+    featureImage.alt = featureLink.title;
+    // All elements have a collection of class names that we can access
+    featureImage.classList.remove('hidden');
+
+    // Stop the normal event from happening
+    evt.preventDefault();
+}
+
+var imgFeatureTitle = document.querySelector('img.feature');
+function changeValue(evt) {
+
+    // var imgFeatureTitle = document.querySelector('img.feature');
+    var paragraph = document.querySelector('p.feature.title');
+    paragraph.innerHTML= featureLink.alt;
+    evt.preventDefault();
+}
+
+
+
+
+// Step 3) Add the function as an event listener (hook it up to the 'click' event)
+featureLink.addEventListener('click', featureLinkHandler);
+//                          \ event / \  method name   /
+//                           \name /
+// Note that the featureLinkHandler is not called right now. Instead, it will be called when the click event happens on the featureLink element.
+imgFeatureTitle.addEventListener('mouseover', changeValue);
