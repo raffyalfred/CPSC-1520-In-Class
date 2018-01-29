8d6fb7343ca68e0237fc3b4db090d6d3ba3d9c69
raffyalfred <35229077+raffyalfred@users.noreply.github.com>
2018-01-29 08:47:20 -0700

    decisions string index

---
+++ Week04/decisions-intro/js/main.js
@@ -0,0 +1,28 @@
+// 1. select the target element
+var featureImage = document.querySelector('img.feature');
+
+// 2. create the event listener function
+function handleFeatureClick() {
+	var desc = document.querySelector('p.feature.description');
+
+	// remove the hidden class IF it is present,
+	// otherwise, add it back!
+	
+	// if(desc.classList.contains('hidden')) {
+	// 	desc.classList.remove('hidden');
+	// } else {
+	// 	desc.classList.add('hidden');
+	// }
+
+	if(desc.className.indexOf('hidden') >= 0) {
+		desc.classList.remove('hidden');
+	} else {
+		desc.classList.add('hidden');
+	}
+	
+	// achieve the same result using using string functions
+	
+}
+
+// 3. add event listener
+featureImage.addEventListener('click', handleFeatureClick);
