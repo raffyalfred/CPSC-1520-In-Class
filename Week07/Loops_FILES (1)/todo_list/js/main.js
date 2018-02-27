28a21c39ec875d57ba3b106fba4101600ab19d4f
raffyalfred <35229077+raffyalfred@users.noreply.github.com>
2018-02-27 11:21:07 -0700

    added search field in loop_files todo_list

--- Week07/Loops_FILES (1)/todo_list/js/main.js
+++ Week07/Loops_FILES (1)/todo_list/js/main.js
@@ -1,5 +1,20 @@
 // Enter JavaScript for the exercise here...
 
 document.querySelector('[name=toggle]').addEventListener('change', function(evt) {
-    console.log('toggle changed');
+    var checks = document.querySelectorAll('[name=message]');
+    var index = 0;
+
+    while (index < checks.length) {
+            if (evt.target.checked) {
+                checks[index].checked = true;
+            } else {
+                checks[index].checked = false;
+            }
+        
+        index = index + 1;
+
+    }
+    
+
+    
 });
---
+++ Week07/todo-items-exercise/todo-items/js/main.js
@@ -0,0 +1,10 @@
+/*
+	Add a 'change' event listener for the select.items drop-down that will create a list of
+	labeled checkboxes based on the value of the option selected.  Start by simply adding 
+	an event listener that logs a message and then search for how to access the selected
+	option's value (this should be very simple so don't get sucked into complex solutions).
+	Perhaps start with the Eloquent JavaScript text and go from there...
+
+	Also, in the img folder there is an image demonstrating the result of choosing
+	a select option from the drop-down.
+*/
