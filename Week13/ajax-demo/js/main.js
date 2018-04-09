861502f109bd4f89a7028b6d54e89c5c107a11c7
raffyalfred <35229077+raffyalfred@users.noreply.github.com>
2018-04-09 09:58:36 -0600

    jquery inclass week13

---
+++ Week13/ajax-demo/js/main.js
@@ -0,0 +1,22 @@
+var xhr = new XMLHttpRequest();
+var button = document.querySelector('.button');
+var output = document.querySelector('.output');
+
+button.addEventListener('click', function (evt) {
+	// use the href for the request URL
+	xhr.open('GET', evt.target.href, true);
+	xhr.send(null);
+	// don't reload the page
+	evt.preventDefault();
+	console.log('finished click event');
+});
+
+xhr.addEventListener('load', function (evt) {
+	if (xhr.status === 200) { // success
+		output.innerHTML = xhr.responseText;
+	} else { // failure
+		console.log('... request failed.');
+		output.innerHTML = '<div>Request Failed.</div>';
+	}
+	console.log('finished load event');
+});
